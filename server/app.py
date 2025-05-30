d655d90e8011b7ffad233582f906f3b245b8aacb
dennis mukuria <denismukuria@gmail.com>
2025-05-31 02:30:57 +0300

    complete all tasks

--- server/app.py
+++ server/app.py
@@ -21,7 +21,32 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def get_earthquake_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
 
+    if earthquake:
+        body = earthquake.to_dict()
+        status = 200
+    
+    else:
+        body = {"message": f"Earthquake {id} not found."}
+        status = 404
+
+    return make_response(body, status)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_earthquake_by_magnitude(magnitude):
+    earthquake = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    quakes = []
+    for quake in earthquake:
+        quakes.append(quake.to_dict())
+    body = {"count": len(quakes),
+            "quakes": quakes
+            }
+    status = 200
+
+    return make_response(body, status)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
